2a08c8d661c7f9411edefad15b0a145d38882691
Abhishek Keri <keri.abhi9@gmail.com>
2025-07-14 20:04:39 -0500

    add tabs

--- app.js
+++ app.js
@@ -60,3 +60,21 @@ searchBooks.addEventListener("keyup", (e) => {
     }
   }
 });
+
+//add tabs
+const tabs = document.querySelector(".tabs");
+const panels = document.querySelectorAll(".panel");
+
+tabs.addEventListener("click", (e) => {
+  if (e.target.tagName === "LI") {
+    //dataset.target cause data-target
+    const targetPanel = document.querySelector(e.target.dataset.target);
+    panels.forEach((panel) => {
+      if (panel === targetPanel) {
+        panel.classList.add("active");
+      } else {
+        panel.classList.remove("active");
+      }
+    });
+  }
+});
